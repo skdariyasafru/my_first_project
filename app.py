8d2846eda00e17bf54a1fa3477b454f2dd481645
skdariyasafru <sk.dariyasafru@gmail.com>
2026-01-13 16:40:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,7 @@
-
-
-from flask import Flask, render_template, request, redirect, session
-import sqlite3
+from flask import Flask
 
 app = Flask(__name__)
-app.secret_key = "secret123"
 
-
-@app.route("/", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
-
-    return render_template("/my_first_project/dashbord.html")
-
-@app.route("/my_first_project/register.html", methods=["GET", "POST"])
-def register():
-    if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
-
-     return render_template("/my_first_project/dashbord.html")
-
-@app.route("/dashboard")
-def dashboard():
-    if "user" in session:
-        return render_template("dashboard.html", user=session["user"])
-    return redirect("/")
-
-@app.route("/logout")
-def logout():
-    session.pop("user", None)
-    return redirect("/")
-
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/")
+def home():
+    return "Hello World"
