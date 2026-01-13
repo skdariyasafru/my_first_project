f3d371ad02c96d40097671330523d95b8787209f
skdariyasafru <sk.dariyasafru@gmail.com>
2026-01-13 20:56:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
 @app.route("/")
 def home():
-    return print(" hrllo rendring is worikun:")
+    return render_template("index.html")
+
 if __name__ == "__main__":
     app.run()
