fb6e19ff50ca2fb08d3a79b3dc8371758fc837b4
skdariyasafru <sk.dariyasafru@gmail.com>
2026-01-13 21:20:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,2 @@
-from flask import Flask, render_template
+print(" hello  runing")
 
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template("/my_first_project/index.html")
-
-if __name__ == "__main__":
-    app.run()
