bfdb9d5dc5e712e89cb907263c143f16b2dbf330
skdariyasafru <sk.dariyasafru@gmail.com>
2026-01-15 19:03:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,20 +4,10 @@ import os
 
 app = Flask(__name__)
 
-# Database connection (Render will provide URL)
-'''DATABASE_URL = os.environ.get("DATABASE_URL")
 
-def get_db_connection():
-    return psycopg2.connect(DATABASE_URL)'''
 
 @app.route("/")
 def home():
-   ''' conn = get_db_connection()
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM users;")
-    users = cur.fetchall()
-    cur.close()
-    conn.close()'''
     return render_template("index.html")
 
 if __name__ == "__main__":
