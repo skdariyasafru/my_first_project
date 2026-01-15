f0f175348d2646e0e7a3f5c05de9a4668fba4492
skdariyasafru <sk.dariyasafru@gmail.com>
2026-01-15 18:21:04 +0530

    app.py

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
-print(" hello  runing")
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello from Python on Render!"
+
+if __name__ == "__main__":
+    app.run()
